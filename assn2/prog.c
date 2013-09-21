6cff638b647f3127e3489f29a1573eb7a636102e
Jordan Davidson <jordan@linux-vjkz.site>
2013-09-20 20:03:57 -0600

    figured out how to print an int to the screen, no conversion is needed

--- assn2/prog.c
+++ assn2/prog.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 int cmpfunc(const void * a, const void * b) {
-	return (*(int*)a - *(int*)b);
+  return (*(int*)a - *(int*)b);
 }
 int main() {
-
+  
+  printf("hello World%d\n\n", 14);
+  return 0;
 }
