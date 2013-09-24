9d78ad35cfbad9c49fe5553878ec793fdd425822
Jordan Davidson <jordan@linux-vjkz.site>
2013-09-23 20:39:48 -0600

    changed formatting so it will print 2 decimals to the screenn

--- assn2/prog.c
+++ assn2/prog.c
@@ -5,19 +5,18 @@ int cmpfunc(const void * a, const void * b) {
   return (*(int*)a - *(int*)b);
 }
 
-int mean(int * items, int count) {
+double mean(int * items, int count) {
   int i;
-  int sum = 0;
+  double sum = 0;
   for(i =0; i < count; i++) {
       sum += items[i];
   }
   return sum / count;
 }
 
-int median(int * items, int count) {
-  int mid = ((float)count / 2.0) + .6;
-  printf("mid %d\n", mid);
-  return items[mid];
+double median(int * items, int count) {
+  int mid = ((double)count / 2.0) + .6;
+  return (double)items[mid];
 }
 int main() {
   int nums[100];
@@ -28,18 +27,18 @@ int main() {
   }
   
   int count = 0;
-  int k;
-  while (fscanf(stdin, "%d", &k) == 1.) {
+  double k;
+  while (fscanf(stdin, "%lf", &k) == 1.) {
     nums[count] = k;
     count ++;
   }
-  //void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
+  //void qsort(void *base, size_t nitems, size_t size, double (*compar)(const void *, const void*))
   qsort(nums, count, sizeof(int), cmpfunc );
   
-  printf("smallest: %d\n", nums[0]);
-  printf("largest: %d\n", nums[count - 1]);
-  printf("mean: %d\n", mean(nums, count));
-  printf("median: %d\n", median(nums, count));
+  printf("Low:%d\n", nums[0]);
+  printf("High:%d\n", nums[count - 1]);
+  printf("Mean:%.2lf\n", mean(nums, count));
+  printf("Median:%.2lf\n", median(nums, count));
   return 0;
 }
 
