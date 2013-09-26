cece3fb606e49befef9c7d82197a92a5394f1883
= <=>
2013-09-26 16:59:18 -0600

    Added isPrime number function to assn4.c

--- assn4/assn4.c
+++ assn4/assn4.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 #include<pthread.h>
+int isPrime(int number) {
+	int i;
+	if ( number < 2 || ( number > && number % 2 == 0)) 
+		return 0;
+	for (i = 3; i * i <= number; i += 2) {
+		if( number % i == 0) {
+			reutrn 0;
+		}
+	}
+	return 1;
+}
+
 int main() {
 	return 0;
 }
