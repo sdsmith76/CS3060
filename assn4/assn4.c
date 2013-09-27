bf131d5f799c3a7f1ab87d146f74078cc95ef34a
= <=>
2013-09-26 18:26:58 -0600

    Fixed many shameful bugs. :(

--- assn4/assn4.c
+++ assn4/assn4.c
@@ -1,34 +1,35 @@
 #include<stdio.h>
 #include<pthread.h>
 int isPrime(int number) {
+	int num = number;
 	int i;
-	if ( number < 2 || ( number > && number % 2 == 0)) 
+	if ( num < 2 || ( num > 2 && num % 2 == 0)) 
 		return 0;
-	for (i = 3; i * i <= number; i += 2) {
-		if( number % i == 0) {
-			reutrn 0;
+	for (i = 3; i * i <= num; i += 2) {
+		if( num % i == 0) {
+			return 0;
 		}
 	}
 	return 1;
 }
 
-int thread_func(void * properties) {
-
+void * thread_func(void * properties) {
+	return NULL;
 }
 struct props {
 	int num;
 	int result;
-}
+};
 
 int main() {
-	count = 0;
+	int count = 0;
 	pthread_t threads[100];
 	int k;
 	while(fscanf(stdin, "%d", &k) == 1) {
-		struct props * p;
-		prop.num = k;
-		pthread_create(&threads[count], null, &thread_func, &p)  
-		count++;		                             #
+		struct props p;
+		p.num = k;
+		pthread_create(&threads[count], NULL, &thread_func, &p);  
+		count++;
 	}
 	return 0;
 }
